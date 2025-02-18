88e0c1f5d2c22b2e991663ea28a5fd1a22cbc467
DiasLuc <diaslucp@gmail.com>
2025-02-17 22:01:01 -0500

    added random values on user spec

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -4,21 +4,24 @@ import DashboardPage from "../pages/dashboardPage.js"
 import MenuPage from "../pages/menuPage.js"
 import MyInfoPage from "../pages/myInfoPage.js"
 
+const Chance = require('chance')
+
+const chance = new Chance()
 const loginPage = new LoginPage()
 const dashboardPage = new DashboardPage()
 const menuPage = new MenuPage()
 const myInfoPage = new MyInfoPage()
 
 describe('User Orange HRM Tests', () => {
-  
+
   it('User Info Update - Success', () => {
     loginPage.accessLoginPage()
     loginPage.loginWithUser(userData.userSuccess.username, userData.userSuccess.password)
     dashboardPage.checkInDashboard()
     menuPage.accessMyInfo()
 
-    myInfoPage.fillPersonalDetails("John", "D.", "Rockefeller")
-    myInfoPage.fillEmployeeDetails("employeeID", "OtherID", "DLNUMBER", "2100-01-05")
+    myInfoPage.fillPersonalDetails(chance.first(), chance.string(), chance.last())
+    myInfoPage.fillEmployeeDetails(chance.integer({min:1, max: 9999999999}), chance.string(), chance.hash({length:8}), "2100-01-05")
     myInfoPage.fillStatus()
     
   })
